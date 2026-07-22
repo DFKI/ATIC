5bd3dfa0005968969f41f74182ff767de4d81244
Markus Schröder <markus.schroeder@dfki.de>
2026-07-22 11:13:29 +0200

    class definition

---
+++ atic-server/src/test/java/de/dfki/sds/aticserver/RdfJsonBridgeUnitTest.java
@@ -0,0 +1,20 @@
+
+
+package de.dfki.sds.aticserver;
+
+import org.junit.jupiter.api.Test;
+ 
+/**
+ *
+ */
+public class RdfJsonBridgeUnitTest {
+
+    public RdfJsonBridgeUnitTest() {
+    }
+    
+    @Test
+    public void test() {
+        
+    }
+
+}
